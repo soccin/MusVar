b212a3d666b520f2aed23802b7402bf2639deb36
Nicholas D. Socci <soccin@mskcc.org>
2024-04-19 20:55:39 -0400

    Fix usage and output folder name

--- bin/deliver.sh
+++ bin/deliver.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 
-if [ "#$" != "1" ]; then
-    echo -e "\n   usage: deliver.sh /path/to/delivery/folder/r_00x\n\n"
+if [ "$#" != "1" ]; then
+    echo -e "\n   usage: deliver.sh /path/to/delivery/folder/r_00x\n"
+    exit
 fi
 
 ODIR=$1
 
-rsync -avP --exclude="markduplicates" --exclude="recalibrated" out $ODIR
+rsync -avP --exclude="markduplicates" --exclude="recalibrated" out/ $ODIR/sarek
 rsync -avP post $ODIR
 
 
