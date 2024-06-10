573f5eb12fb19e6257c9d9948cbe595b65c6f3d0
Nicholas D. Socci <soccin@mskcc.org>
2024-06-10 16:38:13 -0400

    Ignore macOS rsrc files

--- bin/deliver.sh
+++ bin/deliver.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+SDIR=$(dirname "$(readlink -f "$0")")
+
 if [ "$#" != "1" ]; then
     echo -e "\n   usage: deliver.sh /path/to/delivery/folder/r_00x\n"
     exit
@@ -10,4 +12,10 @@ ODIR=$1
 rsync -avP --exclude="markduplicates" --exclude="recalibrated" out/ $ODIR/sarek
 rsync -avP post $ODIR
 
+PROJNO=$(echo $ODIR | tr '/' '\n' | fgrep Proj_ | sed 's/Proj_//')
 
+if [ "$PROJNO" != "" ]; then
+    cat $SDIR/../assets/Email/delivery_email_template.txt \
+        | perl -pe 's/{PROJNO}/'$PROJNO'/g' \
+        | tee DELIVER_EMAIL_${PROJNO}.txt
+fi
