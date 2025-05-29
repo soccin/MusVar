28e4bc3d04736f421959f47928d2b04cfbd6aeef
Nicholas D. Socci <soccin@mskcc.org>
2025-05-29 17:55:54 -0400

    checkpoint

--- src/qcReport0.R
+++ src/qcReport0.R
@@ -1,2 +1,11 @@
+read_as_metrics<-function(ff) {
+    read_tsv(ff,comment="#",show_col_types=F,progress=F) %>%
+        filter(CATEGORY=="PAIR") %>%
+        select(SAMPLE,everything()) %>%
+        select(-LIBRARY,-READ_GROUP,-PCT_HARDCLIP)
+}
+
 require(tidyverse)
-fs::dir_ls("post/metrics",recur=T,regex=".as.txt$")
+asm=fs::dir_ls("post/metrics",recur=T,regex=".as.txt$") %>%
+    map(read_as_metrics) %>%
+    bind_rows
